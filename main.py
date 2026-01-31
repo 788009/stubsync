ac529c1586c8a4e51ed82cd78fc61457d6da943b
788009 <2586738452@qq.com>
2026-01-31 17:56:29 +0800

    chore: add project name to main.py

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import os
 from manager import BackupManager
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Android Incremental Backup Tool")
+    parser = argparse.ArgumentParser(description="StubSync - 维护本地索引的 Android 增量备份工具")
     parser.add_argument('-c', '--config', default='config.toml', help='Path to config file')
     args = parser.parse_args()
     
     if os.path.exists(args.config):
+        print("\nStubSync - 维护本地索引的 Android 增量备份工具\n")
         app = BackupManager(args.config)
         app.run()
     else:
